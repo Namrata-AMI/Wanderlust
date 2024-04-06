755c49b327eec686b9095f09ff2b4b8a41d5a48f
Namrata <namratateshwer@gmail.com>
2024-04-06 20:06:32 +0530

    made new changes

--- app.js
+++ app.js
@@ -82,9 +82,10 @@ passport.deserializeUser(User.deserializeUser());      // to deserialise the use
 
 
 app.use((req,res,next)=>{
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
+   // res.locals.success = req.flash("success");
+   // res.locals.error = req.flash("error");
     res.locals.newUser = req.user;        // here we are storing the current user session //
+    console.log(res.locals.newUser);
     next();
 });
 
